6040c4d348a674bbaa42e8594410b62884f8fdd0
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-04-01 17:30:16 +0200

    add basic date functionality

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,165 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Calendar.module.css';
 
-const Calendar = () => {
+interface ICalendarEvent {
+
+}
+
+interface ICalendarData {
+    date: Date;
+    events: ICalendarEvent[];
+}
+
+interface ICalendarProps {
+    data?: ICalendarData;
+    locale?: Locale;
+    primaryColor?: string;
+    backgroundColor?: string;
+    border?: string;
+    borderRadius?: string;
+    boxShadow?: string;
+    defaultMode?: CalendarMode;
+}
+
+export enum CalendarMode {
+    MONTH = 'month',
+    WEEK = 'week',
+    DAY = 'day',
+}
+
+const Calendar = (props: ICalendarProps) => {
+    const [date, setDate] = useState(props.data?.date ?? new Date());
+    const [mode, setMode] = useState(props.defaultMode ?? CalendarMode.MONTH)
+    const locale = props.locale ?? Locale.EN;
+
     return (
-        <div>
-            <h1>Calendar</h1>
+        <div className={styles.container} style={{
+            color: props.primaryColor ?? 'black',
+            backgroundColor: props.backgroundColor ?? 'white',
+            border: props.border ?? '1px solid black',
+            borderRadius: props.borderRadius ?? '2em',
+            boxShadow: props.boxShadow ?? '0 0 10px rgba(0, 0, 0, 0.1)',
+        }}>
+            {props.data &&
+                <>
+                <div className={styles.header}>
+                    <div>
+                        <button onClick={() => setDate(new Date())}>{Localization[locale].buttons.today}</button>
+                        <button onClick={() => {
+                            switch (mode) {
+                                case CalendarMode.MONTH:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth() - 1, d.getDate()));
+                                    break;
+                                case CalendarMode.WEEK:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() - 7));
+                                    break;
+                                case CalendarMode.DAY:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }}>-</button>
+                        <button onClick={() => {
+                            switch (mode) {
+                                case CalendarMode.MONTH:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth() + 1, d.getDate()));
+                                    break;
+                                case CalendarMode.WEEK:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() + 7));
+                                    break;
+                                case CalendarMode.DAY:
+                                    setDate(d => new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }}>+</button>
+                    </div>
+                    <div>{`${Localization[locale].months[date?.getMonth() ?? 0]} ${date?.getFullYear()}`}</div>
+                    <div>
+                        <button onClick={() => setMode(CalendarMode.MONTH)}>{Localization[locale].buttons.month}</button>
+                        <button onClick={() => setMode(CalendarMode.WEEK)}>{Localization[locale].buttons.week}</button>
+                        <button onClick={() => setMode(CalendarMode.DAY)}>{Localization[locale].buttons.day}</button>
+                    </div>
+                </div>
+                <div>
+                <table>
+                        <thead>
+                            <tr>
+                                {Localization[locale].days.map((day, index) => (
+                                    <th key={index}>{day}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                </>
+                
+            }
+            {!props.data &&
+                <div>
+                    <p style={
+                        {
+                            color: props.primaryColor ?? 'black',
+                            fontSize: '2em',
+                            fontWeight: 'bold',
+                        }
+                    }>
+                        {Localization[locale].messages.no_data}
+                    </p>
+                </div>
+            }
         </div>
     );
 };
 
 export default Calendar;
+
+export enum Locale {
+    EN = 'en',
+    CZ = 'cz',
+    RU = 'ru',
+}
+
+const Localization = {
+    [Locale.EN]: {
+        days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        messages: {
+            no_data: 'This calendar has no data.',
+        },
+        buttons: {
+            today: 'Today',
+            month: 'Month',
+            week: 'Week',
+            day: 'Day',
+        },
+    },
+    [Locale.CZ]: {
+        days: ['Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne'],
+        months: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+        messages: {
+            no_data: 'Tento kalendář nemá žádná data.',
+        },
+        buttons: {
+            today: 'Dnes',
+            month: 'Měsíc',
+            week: 'Týden',
+            day: 'Den',
+        },
+    },
+    [Locale.RU]: {
+        days: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        messages: {
+            no_data: 'В этом календаре нет данных.',
+        },
+        buttons: {
+            today: 'Сегодня',
+            month: 'Месяц',
+            week: 'Неделя',
+            day: 'День',
+        },
+    },
+};
