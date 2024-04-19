26e089e34ff9e1f5888cc23b9149738c6687e7b5
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-04-19 22:44:59 +0200

    finish the reducer

--- src/components/Calendar/Context.tsx
+++ src/components/Calendar/Context.tsx
@@ -40,19 +40,52 @@ const reducer = (state: ICalendarData, action: ReducerAction) => {
                 ...state,
                 events: [...state.events, action.event],
             };
+        case ActionType.CLEAR_EVENTS:
+            return {
+                ...state,
+                events: [],
+            };
+        case ActionType.SET_DATA:
+            return action.data;
+        case ActionType.SET_DATE:
+            return {
+                ...state,
+                date: action.date,
+            };
+        case ActionType.SET_MONTH:
+            return {
+                ...state,
+                date: new Date(state.date.getFullYear(), action.month),
+            };
+        case ActionType.SET_WEEK:
+            return {
+                ...state,
+                date: new Date(state.date.getFullYear(), state.date.getMonth(), action.week * 7),
+            };
+        case ActionType.SET_DAY:
+            return {
+                ...state,
+                date: new Date(state.date.getFullYear(), state.date.getMonth(), action.day),
+            };
         default:
             return state;
     }
 };
 
 type ReducerAction = 
-{ type: ActionType.ADD_EVENT, event: ICalendarEvent }
+{ type: ActionType.ADD_EVENT, event: ICalendarEvent } |
+{ type: ActionType.CLEAR_EVENTS } |
+{ type: ActionType.SET_DATA, data: ICalendarData } |
+{ type: ActionType.SET_DATE, date: Date } |
+{ type: ActionType.SET_MONTH, month: number } |
+{ type: ActionType.SET_WEEK, week: number } |
+{ type: ActionType.SET_DAY, day: number };
 
 export enum ActionType {
     ADD_EVENT = 'ADD_EVENT',
-    REMOVE_EVENT = 'REMOVE_EVENT',
+    CLEAR_EVENTS = 'REMOVE_EVENT',
+    SET_DATA = 'SET_DATA',
     SET_DATE = 'SET_DATE',
-    SET_EVENTS = 'SET_EVENTS',
     SET_MONTH = 'SET_MONTH',
     SET_WEEK = 'SET_WEEK',
     SET_DAY = 'SET_DAY',
