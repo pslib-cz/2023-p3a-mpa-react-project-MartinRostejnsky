649e32b79bc1f35cb568e6e3059a2e2cd44b0b4e
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-04-28 15:06:09 +0200

    lil bit of date comparison

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -53,9 +53,9 @@ const Calendar = (props: ICalendarProps) => {
 
     useEffect(() => {
         dispatch({ type: ActionType.SET_DATA, data: /*props.data ?? */{ date: new Date(), events: [
-            { title: "fotoaparat", start: new Date('2024-04-26'), end: new Date('2024-04-29'), row: 0, color: 'red'},
-            { title: "fotoaparat2", start: new Date('2024-04-17'), end: new Date('2024-04-19'), row: 0, color: 'blue'},
-            { title: "fotoaparat3", start: new Date('2024-04-24'), end: new Date('2024-04-28'), row: 1, color: 'green'},
+            { title: "fotoaparat", start: new Date('2024-04-26'), end: new Date('2024-04-29'), row: 0, color: '#C3C9FF'},
+            { title: "fotoaparat2", start: new Date('2024-04-17'), end: new Date('2024-04-19'), row: 0, color: '#5BC0DE'},
+            { title: "fotoaparat3", start: new Date('2024-04-24'), end: new Date('2024-04-28'), row: 1, color: '#83C291'},
         ] }});
     }, [props.data])
     useEffect(() => {
--- src/components/Calendar/Parts.tsx
+++ src/components/Calendar/Parts.tsx
@@ -3,6 +3,13 @@ import { Localization } from './Localization';
 import { ActionType, CalendarContext } from './Context';
 import styles from './Calendar.module.css';
 
+const compareDates = (date1: Date, date2: Date) => {
+    console.log(date1.getFullYear() === date2.getFullYear(), "year");
+    console.log(date1.getMonth() === date2.getMonth(), "month");
+    console.log(date1.getDate() === date2.getDate(), "day", date1.getDate(), date2.getDate());
+    return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
+}
+
 const DateIndicator = () => {
     const { 
         locale,
@@ -51,7 +58,12 @@ const TableField = ({date}: {date: Date}) => {
 
     const sortedEvents = calendarEvents.sort((a, b) => a.row - b.row);
     const lastRow = sortedEvents.length > 0 ? sortedEvents[sortedEvents.length - 1].row : 0;
-    const currentEvents = sortedEvents.filter(event => date <= event.end && date >= event.start);
+    const currentEvents = sortedEvents.filter(event => {
+        const eventStartDate = new Date(event.start.getFullYear(), event.start.getMonth(), event.start.getDate());
+        const eventEndDate = new Date(event.end.getFullYear(), event.end.getMonth(), event.end.getDate());
+        const currentDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+        return currentDate >= eventStartDate && currentDate <= eventEndDate;
+    });
 
     console.log(date)
     console.log(calendarEvents)
@@ -64,15 +76,19 @@ const TableField = ({date}: {date: Date}) => {
             <p>{date.getDate()}</p>
             {currentEvents.length > 0 && Array.from({ length: lastRow+1 }).map((_, index) => { 
                 const currentEvent = currentEvents.find(event => event.row === index);
-                const test = (
+                const eventBar = (
                     <div 
-                    className={styles.table__field__event} 
+                    className={`${styles.table__field__event} ${currentEvent && compareDates(currentEvent.start, date) ? styles["table__field__event--start"] : ''}`} 
                     style={{backgroundColor: currentEvent?.color, order: index}} key={index}>{currentEvent?.title}</div>
                 )
-                console.log(test)
-                console.log(index)
-                console.log(currentEvent)
-                return test; })}
+                
+                if (currentEvent && currentEvent.start) {
+                
+                    console.log(compareDates(currentEvent.start, date))
+                }
+
+                return eventBar; 
+                })}
         </div>
     )
 }
