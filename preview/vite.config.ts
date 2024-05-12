01abac20f586f7736bfaed089abd3ea6a30692eb
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-05-12 16:38:06 +0200

    base

--- preview/vite.config.ts
+++ preview/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2023-p3a-react-project-MartinRostejnsky/',
+  base: '/preview/',
 })
